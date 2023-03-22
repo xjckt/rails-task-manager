b2fe96dc8252eec2be31492992c2e843686a7e8d
Jonathan To <jonto94@gmail.com>
2023-03-22 22:14:39 +1100

    added updated and delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
+  # create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # read
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
